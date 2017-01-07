6657b1b930291de95a818b685ac963d44ed1c30d
qiaoyu <qiaoyu1111358@gmail.com>
2017-01-07 13:18:48 +0800

    完成jobs#edit与jobs#update的功能

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,6 +11,19 @@ class JobsController < ApplicationController
     @job = Job.new
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
   def create
     @job = Job.new(job_params)
 
